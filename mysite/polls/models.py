c1d7e5bad3c42297b0cefa7cc85298657bb31589
MaxOvcharov <ovcharovmax@yandex.ru>
2016-11-13 16:10:45 +0300

    Add new app polls

---
+++ mysite/polls/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    voite = models.IntegerField()
