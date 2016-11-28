cd15663c78efc26b54888bc0f1221c0f97a12af0
MaxOvcharov <ovcharovmax@yandex.ru>
2016-11-28 23:35:40 +0300

    Added new template for detail controller

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse, Http404
 from .models import Question
-from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request,
-                             {'latest_question_list': latest_question_list, })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("<b>You are looking at the result of question {0}</b>".format(question_id))
+    # Более длинная версия
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question doesn't exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question,
+                                                 "question_id": question_id})
 
 
 def results(request, question_id):
